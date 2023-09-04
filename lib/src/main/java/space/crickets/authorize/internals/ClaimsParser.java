1d59953c63170b04f8e9c19b787ced0a029f5655
Jatin Sonavane <jatins@hey.com>
2023-09-04 08:16:38 -0700

    Provides failing tests for the to-be-implemented @Authorize annotation.

---
+++ lib/src/main/java/space/crickets/authorize/internals/ClaimsParser.java
@@ -0,0 +1,14 @@
+package space.crickets.authorize.internals;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.stereotype.Component;
+
+/**
+ * TODO implement me
+ */
+@Component
+public class ClaimsParser {
+    public Claims parse(String authorization) {
+        return null;
+    }
+}
